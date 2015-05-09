f2d054a8aed0f28b2e309333d323f19503b76f70
Teemu Kanstren <teemu@kanstren.net>
2015-05-09 17:06:45 +0300

    few notes on snmp for future

--- src/pypro/snmp/main.py
+++ src/pypro/snmp/main.py
@@ -2,12 +2,16 @@ __author__ = 'teemu kanstren'
 
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
+#resource OID's on linux: http://www.debianadmin.com/linux-snmp-oids-for-cpumemory-and-disk-statistics.html
+#snmpwalk = get the whole subtree from given node
+#snmpgetnext = get next for given value in tree. looping gives walk.
+
 cmdGen = cmdgen.CommandGenerator()
 
 errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-    cmdgen.CommunityData('public'),
-    cmdgen.UdpTransportTarget(('demo.snmplabs.com', 161)),
-    cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0),
+    cmdgen.CommunityData('get_c'),
+    cmdgen.UdpTransportTarget(('192.168.2.1', 161)),
+    '1.3.6.1.4.1.2021.11.50.0',
     lookupNames=True, lookupValues=True
 )
 
