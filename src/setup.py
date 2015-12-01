ab8aa6bbfbf5cd7d6c5e596f04b68a0069f8d594
Teemu Kanstren <tkanstren@gmail.com>
2015-12-01 22:24:36 +0200

    updating deployment script

--- src/setup.py
+++ src/setup.py
@@ -2,21 +2,22 @@ __author__ = 'teemu kanstren'
 
 from setuptools import setup
 
-setup(name='pypro-local',
-      version='0.5.2',
+setup(name='pypro-snmp',
+      version='0.3.4',
       description='System resource probes',
       url='https://github.com/mukatee/pypro',
       author='Teemu Kanstren',
       author_email='tkanstren@gmail.com',
       license='MIT',
-      packages=['pypro', 'pypro.local', 'pypro.local.loggers'],
+      packages=['pypro', 'pypro.snmp', 'pypro.snmp.loggers', 'pypro.snmp.oids'],
+      package_data={'pypro.snmp': ['*.avsc']},
       zip_safe=True,
       install_requires=[
-          'psutil', 'elasticsearch', 'kafka-python'
+          'psutil', 'elasticsearch', 'kafka-python', 'pysnmp', 'avro-python3', 'influxdb'
       ],
       classifiers=['License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 3.4',
                    'Development Status :: 4 - Beta',
-                   'Topic :: System :: Monitoring'],
-      keywords='monitoring'
+                   'Topic :: System :: Networking :: Monitoring'],
+      keywords='monitoring',
      )
--- src/setup_snmp.py
+++ src/setup_snmp.py
@@ -3,16 +3,17 @@ __author__ = 'teemu kanstren'
 from setuptools import setup
 
 setup(name='pypro-snmp',
-      version='0.3.0',
+      version='0.3.4',
       description='System resource probes',
       url='https://github.com/mukatee/pypro',
       author='Teemu Kanstren',
       author_email='tkanstren@gmail.com',
       license='MIT',
-      packages=['pypro', 'pypro.snmp', 'pypro.snmp.loggers'],
+      packages=['pypro', 'pypro.snmp', 'pypro.snmp.loggers', 'pypro.snmp.oids'],
+      package_data={'pypro.snmp': ['*.avsc']},
       zip_safe=True,
       install_requires=[
-          'psutil', 'elasticsearch', 'kafka-python', 'pysnmp', 'avro', 'influxdb'
+          'psutil', 'elasticsearch', 'kafka-python', 'pysnmp', 'avro-python3', 'influxdb'
       ],
       classifiers=['License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 3.4',
